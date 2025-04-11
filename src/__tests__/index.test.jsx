95a6976f810608c6385f5d4762297a1c6fb0828f
vrotondo <vrfr91@gmail.com>
2025-04-10 20:41:17 -0400

    Your message here

--- src/__tests__/index.test.jsx
+++ src/__tests__/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 import { sampleProducts } from '../utils/helpers';
 
 test('renders shopping app', () => {
